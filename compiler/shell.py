5fab887ba4bccd37f14b133d69d68e5c0421f649
Abhishek Kaushik <kaushikdeveloper759@gmail.com>
2021-02-23 22:50:20 +0530

    Edited the shell file to integrate with the backend

--- compiler/shell.py
+++ compiler/shell.py
@@ -11,36 +11,59 @@ response = {
     "symbol_table": None
 }
 
+result = {
+    "status" : "",
+    "data" : None
+}
 
-def comviz(inp):
-    lexer_result, lexer_errors, ast_root, syntax_errors, eval_result, runtime_errors = run(file_name='<stdin>',
-                                                                                           text=inp)
 
-    if lexer_errors:
-        print(lexer_errors.as_string())
-        response["lexer_errors"] = lexer_errors.as_string()
-    else:
-        # No lexical errors
-        print(lexer_result)
-        response["tokens"] = lexer_result
+def comviz(source_code):
+    try:
+        lexer_result, lexer_errors, ast_root, syntax_errors, eval_result, runtime_errors = run(file_name='<stdin>',
+                                                                                           text=source_code)
 
-        if syntax_errors:
-            print(syntax_errors.as_string())
-            response["syntax_errors"] = syntax_errors.as_string()
+        if lexer_errors:
+            print(lexer_errors.as_string())
+            response["lexer_errors"] = lexer_errors.as_string()
+
+            result['status'] = 'error'
+            result['data'] = str(response)
         else:
-            # No syntax errors
-            print(ast_root)
-            response["digraphs"] = pt_digraphs
+            # No lexical errors
+            print(lexer_result)
+            response["tokens"] = lexer_result
 
-            if runtime_errors:
-                print(runtime_errors.as_string())
-                response["runtime_error"] = runtime_errors.as_string()
+            if syntax_errors:
+                print(syntax_errors.as_string())
+                response["syntax_errors"] = syntax_errors.as_string()
+
+                result['status'] = 'error'
+                result['data'] = str(response)
             else:
-                # No runtime errors
-                print(eval_result)
-                response["evaluation_result"] = eval_result
-                response["symbol_table"] = global_symbol_table
-                print(response)
+                # No syntax errors
+                print(ast_root)
+                response["digraphs"] = pt_digraphs
 
-    return response
-comviz("1+2")
+                if runtime_errors:
+                    print(runtime_errors.as_string())
+                    response["runtime_error"] = runtime_errors.as_string()
+
+                    result['status'] = 'error'
+                    result['data'] = str(response)
+                else:
+                    # No runtime errors
+                    print(eval_result)
+                    response["evaluation_result"] = eval_result
+                    response["symbol_table"] = global_symbol_table
+                    print(response)
+
+                    result['status'] = 'success'
+                    result['data'] = str(response)
+
+        return result
+    except Exception as e:
+        print("An error occurred : "+str(e))
+        
+        result['status'] = "error"
+        result['data'] = str(e)
+        return result
